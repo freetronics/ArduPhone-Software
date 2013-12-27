0a4fef3647eb1599949e7ead90d0777652b93551
Cary Dreelan <cary@active-ideas.com.au>
2013-12-28 01:15:00 +1030

    Be consistent with constant naming

--- firmware_v1/keypad.ino
+++ firmware_v1/keypad.ino
@@ -9,22 +9,22 @@
 
 // === Constants ===
 
-const byte keypad_ROWS = 6 ;
-const byte keypad_COLS = 3 ;
+const byte KEYPAD_ROWS = 6 ;
+const byte KEYPAD_COLS = 3 ;
 const byte checkKeyPeriod = 5 ; // in ms
 
 // === States ===
 
 enum keypadStates {
-  keypad_UNINITIALISED,
-  keypad_SETUP,
-  keypad_IDLE
+  KEYPAD_UNINITIALISED,
+  KEYPAD_SETUP,
+  KEYPAD_IDLE
 } ;
 keypadStates keypadState ;
 
 // === Variables ===
 
-char keys[ keypad_ROWS ][ keypad_COLS ] = {
+char keys[ KEYPAD_ROWS ][ KEYPAD_COLS ] = {
   {'!','U','?'},
   {'L','D','R'},
   {'1','2','3'},
@@ -32,9 +32,9 @@ char keys[ keypad_ROWS ][ keypad_COLS ] = {
   {'7','8','9'},
   {'*','0','#'}
 } ;
-byte keypadRowPins[ keypad_ROWS ] = { A6, A4, A3, A2, A1, A0 } ;  // 25, 27, 28, 29, 30, 31
-byte keypadColPins[ keypad_COLS ] = { A5, A7, 23 } ;              // 26, 24, 23
-Keypad keypad = Keypad( makeKeymap( keys ), keypadRowPins, keypadColPins, keypad_ROWS, keypad_COLS ) ;
+byte keypadRowPins[ KEYPAD_ROWS ] = { A6, A4, A3, A2, A1, A0 } ;  // 25, 27, 28, 29, 30, 31
+byte keypadColPins[ KEYPAD_COLS ] = { A5, A7, 23 } ;              // 26, 24, 23
+Keypad keypad = Keypad( makeKeymap( keys ), keypadRowPins, keypadColPins, KEYPAD_ROWS, KEYPAD_COLS ) ;
 unsigned long lastCheckKeyTime ;
 
 // === Functions ===
@@ -57,15 +57,15 @@ void KeypadSetup() {
 
 void KeypadSlice() {
   switch ( keypadState ) {
-    case keypad_UNINITIALISED :
-      keypadState = keypad_SETUP ;
+    case KEYPAD_UNINITIALISED :
+      keypadState = KEYPAD_SETUP ;
       break ;
       
-    case keypad_SETUP :
-      keypadState = keypad_IDLE ;
+    case KEYPAD_SETUP :
+      keypadState = KEYPAD_IDLE ;
       break ;
       
-    case keypad_IDLE :
+    case KEYPAD_IDLE :
       checkKeyPressed() ;
       break ;
   }
